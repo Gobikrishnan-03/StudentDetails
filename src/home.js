3fea70dc327e5cc3ac2ea0172e6d1ddb9c403281
Gobikrishnan03 <gokulcse1856@gmail.com>
2021-04-19 14:45:46 +0530

    first commit

---
+++ src/home.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+$('#myCarousel').carousel({
+    interval: 4000
+})
+
+$('.carousel .item').each(function() {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < 2; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
